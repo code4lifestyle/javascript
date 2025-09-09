2b9818f0cf7dd38b60473acd2af3578ec723ce0b
Code4life <code4lifestyle@gmail.com>
2025-09-10 04:26:43 +0500

    add opPre and operation

---
+++ part1/logical.js
@@ -0,0 +1,11 @@
+    // && -> and
+    // || -> or
+    // ! -> reverse
+
+    let isLogedIn = true
+    let paidUser = false 
+    // console.log(isLogedIn && paidUser)
+
+    let isEmailuser = true ;
+    let isGoogleuser = false ;
+    console.log( isEmailuser || isGoogleuser);
---
+++ part1/operations.js
@@ -0,0 +1,26 @@
+let score = 100;
+let bonus = 21.85
+let totalScore = score + bonus
+console.log(totalScore);
+// console.log(addition)
+
+let addition = 4 + 5 ;
+let subtract = 5 - 2 ; 
+let multipl = 5 * 3 ;
+let div = 15 / 5 ;
+let reminder = 16 % 5 ;
+let expo = 2 ** 4 ;
+let myScore = 100
+// increament
+myScore ++
+// decreament
+myScore--
+
+// comparion operation
+
+let num1 = 3
+let num2 = 3
+let num3 = 6
+console.log(num1 == num2);
+console.log(num1 != num3);
+console.log(num1 != num2);
