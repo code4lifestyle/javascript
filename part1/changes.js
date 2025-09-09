09daab183297e8c791a50a150411dfbaa46fcb8d
Code4life <code4lifestyle@gmail.com>
2025-09-10 03:43:14 +0500

    changes

---
+++ part1/changes.js
@@ -0,0 +1,8 @@
+let gameName = "spiderman";
+getName = "Batman"
+console.log(getName);
+
+// const userName = 'jhon'
+// userName = 'someoneElse'
+// console.log(userName);
+// value of const can't be replaced 
