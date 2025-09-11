15df84d49a5825d548e6387c4a2daf0ad1b87bc5
Code4life <code4lifestyle@gmail.com>
2025-09-12 01:02:11 +0500

    premitive

---
+++ part2/premitive.js
@@ -0,0 +1,20 @@
+let balance = 120
+// console.log(typeof(balance));
+let anothereBalance = new Number(129)
+// console.log(typeof balance);
+// console.log(typeof anothereBalance)
+let  isActive = true;
+const isRealyactive = new Boolean (true);
+// console.log(typeof isActive, typeof isRealyactive);
+let age;
+// console.log(typeof age)
+let firstName = undefined;
+let lastName = null; 
+// console.log( typeof lastName  )
+// console.log(lastName);
+
+// string 
+let string = "hello world"
+let myStringone = 'hola'
+let userName = 'shzd-sha'
+console.log(typeof string, myStringone, userName);
