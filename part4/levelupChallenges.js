9e3b560ade21acf13e51457b4d9b3e586576d191
Code4life <azharzharmehmoodm@gmail.com>
2025-09-16 19:00:12 +0500

    changes

--- part4/levelupChallenges.js
+++ part4/levelupChallenges.js
@@ -1,18 +1,26 @@
-let Teas = ['greentea', 'blacktea', 'chai', 'oolangtea'];
-let selectedtea = [];
-for(i=0; i<Teas.length; i++){
-    if(Teas[i]==='chai'){
-        break;
-    }
-    selectedtea.push(Teas[i]);
+// let Teas = ['greentea', 'blacktea', 'chai', 'oolangtea'];
+// let selectedtea = [];
+// for(i=0; i<Teas.length; i++){
+//     if(Teas[i]==='chai'){
+//         break;
+//     }
+//     selectedtea.push(Teas[i]);
 
-}
-console.log(selectedtea);
-let cities = ['london','newyork','paris','berlin']
-let cityVisited = []
-for(i=0; i<cities.length; i++){
-    if(cities[i]!=='paris'){
-        cityVisited.push(cities[i]);
-    }
-}
-console.log(cityVisited);
+// }
+// console.log(selectedtea);
+// let cities = ['london','newyork','paris','berlin']
+// let cityVisited = []
+// for(i=0; i<cities.length; i++){
+//     if(cities[i]!=='paris'){
+//         cityVisited.push(cities[i]);
+//     }
+// }
+// console.log(cityVisited);
+// let cities = ["london", 'paris', "newyork", 'koyoto']
+// let favCities = []
+// for(i=0; i<cities.length; i++ ){
+//     if(cities[i] !=='paris'){
+//         favCities.push(cities[i])
+//     }
+// }
+// console.log(favCities);
