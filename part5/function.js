f5bc42e7340e85bba73cd6e160b183c395235843
Code4life <code4lifestyle@gmail.com>
2025-09-17 03:34:24 +0500

    functions

---
+++ part5/function.js
@@ -0,0 +1,19 @@
+function makeTea(typeofTea){
+    return `making ${typeofTea}`
+}
+let greet = makeTea('greenTea');
+// console.log(greet);
+function OrderTea(Teatype){
+    function ConfirmOrder(){
+        return`order Confirmed for Chai`
+    }
+    return ConfirmOrder();
+}
+let OrderConfirmation = OrderTea('chai')
+// console.log(OrderConfirmation);
+
+const CalculateTotal = (price, quantity) => {
+return price * quantity
+}
+let totalCast = CalculateTotal(599 , 299);
+console.log(totalCast)
