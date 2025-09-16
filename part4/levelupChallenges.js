4dedd9da68f13327d296d7c3a2da3050e3188064
Code4life <code4lifestyle@gmail.com>
2025-09-16 21:28:00 +0500

    changes forof loop

--- part4/levelupChallenges.js
+++ part4/levelupChallenges.js
@@ -1,39 +1,56 @@
-let Teas = ['greentea', 'blacktea', 'chai', 'oolangtea'];
-let selectedtea = [];
-for(i=0; i<Teas.length; i++){
-    if(Teas[i]==='chai'){
-        break;
-    }
-    selectedtea.push(Teas[i]);
+// let Teas = ['greentea', 'blacktea', 'chai', 'oolangtea'];
+// let selectedtea = [];
+// for(i=0; i<Teas.length; i++){
+//     if(Teas[i]==='chai'){
+//         break;
+//     }
+//     selectedtea.push(Teas[i]);
 
+// }
+// console.log(selectedtea);
+// let cities = ['london','newyork','paris','berlin']
+// let cityVisited = []
+// for(i=0; i<cities.length; i++){
+//     if(cities[i]!=='paris'){
+//         cityVisited.push(cities[i]);
+//     }
+// }
+// console.log(cityVisited);
+// let cities = ["london", 'paris', "newyork", 'koyoto']
+// let favCities = []
+// for(i=0; i<cities.length; i++ ){
+//     if(cities[i] !=='paris'){
+//         favCities.push(cities[i])
+//     }
+// }
+// console.log(favCities);
+// const campaigns = [
+//   { name: "car", employees: ["azhar", "sohaib", "shehzad"] },
+//   { name: "health", employees: ["umar", "shbaz", "ali"] },
+//   { name: "mortage", employees: ["umar", "wahid", "kamran"] }
+// ];
+// const selectedCampaigns = [];
+// for (let i = 0; i < campaigns.length; i++) {
+//     if(campaigns[i].name!=="car"){
+//        selectedCampaigns.push(campaigns[i]);
+//     }
+// }
+// console.log(selectedCampaigns);
+// let cities = ['london', 'paris','tokyo', 'islamabad']
+// let favCities = [];
+// for(i=0; i<cities.length; i++){
+//     if(cities[i]!=='islamabad'){
+//         favCities.push(cities[i])
+//     }
+    
+// }
+let numbers = [1,2,3,4,5,6]
+let smallNumber = []
+for (const number of numbers) {
+    if(number === 4){
+       break;
+    } 
+    smallNumber.push(number)
+    
 }
-console.log(selectedtea);
-let cities = ['london','newyork','paris','berlin']
-let cityVisited = []
-for(i=0; i<cities.length; i++){
-    if(cities[i]!=='paris'){
-        cityVisited.push(cities[i]);
-    }
-}
-console.log(cityVisited);
-let cities = ["london", 'paris', "newyork", 'koyoto']
-let favCities = []
-for(i=0; i<cities.length; i++ ){
-    if(cities[i] !=='paris'){
-        favCities.push(cities[i])
-    }
-}
-console.log(favCities);
-const campaigns = [
-  { name: "car", employees: ["azhar", "sohaib", "shehzad"] },
-  { name: "health", employees: ["umar", "shbaz", "ali"] },
-  { name: "mortage", employees: ["umar", "wahid", "kamran"] }
-];
-const selectedCampaigns = [];
-for (let i = 0; i < campaigns.length; i++) {
-    if(campaigns[i].name!=="car"){
-       selectedCampaigns.push(campaigns[i]);
-    }
-}
-console.log(selectedCampaigns);
-let cities
+console.log(smallNumber)
