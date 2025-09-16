13ac2c790d330f7cb86c33ea1783b489ac643bec
Code4life <code4lifestyle@gmail.com>
2025-09-16 20:40:02 +0500

    new changes

--- part4/levelupChallenges.js
+++ part4/levelupChallenges.js
@@ -1,39 +1,39 @@
-// let Teas = ['greentea', 'blacktea', 'chai', 'oolangtea'];
-// let selectedtea = [];
-// for(i=0; i<Teas.length; i++){
-//     if(Teas[i]==='chai'){
-//         break;
-//     }
-//     selectedtea.push(Teas[i]);
+let Teas = ['greentea', 'blacktea', 'chai', 'oolangtea'];
+let selectedtea = [];
+for(i=0; i<Teas.length; i++){
+    if(Teas[i]==='chai'){
+        break;
+    }
+    selectedtea.push(Teas[i]);
 
-// }
-// console.log(selectedtea);
-// let cities = ['london','newyork','paris','berlin']
-// let cityVisited = []
-// for(i=0; i<cities.length; i++){
-//     if(cities[i]!=='paris'){
-//         cityVisited.push(cities[i]);
-//     }
-// }
-// console.log(cityVisited);
-// let cities = ["london", 'paris', "newyork", 'koyoto']
-// let favCities = []
-// for(i=0; i<cities.length; i++ ){
-//     if(cities[i] !=='paris'){
-//         favCities.push(cities[i])
-//     }
-// }
-// console.log(favCities);
-// const campaigns = [
-//   { name: "car", employees: ["azhar", "sohaib", "shehzad"] },
-//   { name: "health", employees: ["umar", "shbaz", "ali"] },
-//   { name: "mortage", employees: ["umar", "wahid", "kamran"] }
-// ];
-// const selectedCampaigns = [];
-// for (let i = 0; i < campaigns.length; i++) {
-//     if(campaigns[i].name!=="car"){
-//        selectedCampaigns.push(campaigns[i]);
-//     }
-// }
-// console.log(selectedCampaigns);
+}
+console.log(selectedtea);
+let cities = ['london','newyork','paris','berlin']
+let cityVisited = []
+for(i=0; i<cities.length; i++){
+    if(cities[i]!=='paris'){
+        cityVisited.push(cities[i]);
+    }
+}
+console.log(cityVisited);
+let cities = ["london", 'paris', "newyork", 'koyoto']
+let favCities = []
+for(i=0; i<cities.length; i++ ){
+    if(cities[i] !=='paris'){
+        favCities.push(cities[i])
+    }
+}
+console.log(favCities);
+const campaigns = [
+  { name: "car", employees: ["azhar", "sohaib", "shehzad"] },
+  { name: "health", employees: ["umar", "shbaz", "ali"] },
+  { name: "mortage", employees: ["umar", "wahid", "kamran"] }
+];
+const selectedCampaigns = [];
+for (let i = 0; i < campaigns.length; i++) {
+    if(campaigns[i].name!=="car"){
+       selectedCampaigns.push(campaigns[i]);
+    }
+}
+console.log(selectedCampaigns);
 let cities
