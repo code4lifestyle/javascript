e2c752ea7a56a8715f998f11fb9fbbb79bac3aa2
Code4life <code4lifestyle@gmail.com>
2025-09-17 22:53:14 +0500

    changes

--- part5/function.js
+++ part5/function.js
@@ -1,38 +1,56 @@
-function makeTea(typeofTea){
-    return `making ${typeofTea}`
-}
-let greet = makeTea('greenTea');
-// console.log(greet);
-function OrderTea(Teatype){
-    function ConfirmOrder(){
-        return`order Confirmed for Chai`
-    }
-    return ConfirmOrder();
-}
-let OrderConfirmation = OrderTea('chai')
-// console.log(OrderConfirmation);
+// function makeTea(typeofTea){
+//     return `making ${typeofTea}`
+// }
+// let greet = makeTea('greenTea');
+// // console.log(greet);
+// function OrderTea(Teatype){
+//     function ConfirmOrder(){
+//         return`order Confirmed for Chai`
+//     }
+//     return ConfirmOrder();
+// }
+// let OrderConfirmation = OrderTea('chai')
+// // console.log(OrderConfirmation);
 
-const CalculateTotal = (price, quantity) => {
-return price * quantity
-}
-let totalCast = CalculateTotal(599 , 299);
-// console.log(totalCast)
-function makeTea(typeofTea){
-    return `tea is ${typeofTea}`
-}
+// const CalculateTotal = (price, quantity) => {
+// return price * quantity
+// }
+// let totalCast = CalculateTotal(599 , 299);
+// // console.log(totalCast)
+// function makeTea(typeofTea){
+//     return `tea is ${typeofTea}`
+// }
 
-function processTeaOrder(tea){
- return tea('earlgrey')
-}
-let order = processTeaOrder(makeTea);
-// console.log(order)
+// function processTeaOrder(tea){
+//  return tea('earlgrey')
+// }
+// let order = processTeaOrder(makeTea);
+// // console.log(order)
 
-function createTeamaker (){
-    return function(teaType){
-        return `making ${teaType}`
-    }
+// function createTeamaker (){
+//     return function(teaType){
+//         return `making ${teaType}`
+//     }
+// }
+// let Teamaker = createTeamaker();
+// // console.log(Teamaker('greeting'));
+// let result = Teamaker('greeting tea');
+// // console.log(result)
+// function Aa (name){
+// return (`hello ${name}`)
+// }
+// const namee = Aa('coffe');
+// console.log(namee);
+// function orderTea (teaType){
+//     function cofirmOrder() {
+//         return `order confirmed for Chai`;
+//     }
+//     return cofirmOrder();
+// }
+// let orderConfirmation = orderTea('chai');
+// console.log(orderConfirmation);
+const CalculateTotal = (price, qunatity) => {
+return price * qunatity
 }
-let Teamaker = createTeamaker();
-// console.log(Teamaker('greeting'));
-let result = Teamaker('greeting tea');
-console.log(result)
+let totalCost = CalculateTotal(400, 30);
+console.log(totalCost);
