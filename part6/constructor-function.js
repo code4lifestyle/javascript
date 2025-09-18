dab965d03873b1028bb0735a098b136b29283079
Code4life <code4lifestyle@gmail.com>
2025-09-19 00:34:19 +0500

    changes

--- part6/constructor-function.js
+++ part6/constructor-function.js
@@ -20,22 +20,6 @@
 // console.log(lemonTea.describe());
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // function Tea(type){
 //     this.type = type ;
 //     this.describe = function(){
@@ -45,22 +29,44 @@
 // let TeaType = new Tea('black tea')
 // console.log(TeaType.describe())
 
-function Teatype(type) {
-    this.type = "any"; 
-    this.des = function() {
-        return `the key name is ${this.type}`;
+// function Teatype(type) {
+//     this.type = "any"; 
+//     this.des = function() {
+//         return `the key name is ${this.type}`;
+//     }
+// }
+
+// let a = new Teatype();
+// // console.log(a.des());
+
+
+// function Animal(species){
+//     this.species = species
+// }
+// Animal.prototype.sound = function(){
+//     return `${this.species} make a sound`
+// }
+// let dog = new Animal('dog');
+// console.log(dog.sound());
+
+function Person (name,age){
+    this.name = name
+    this.age = age
+}
+function Car(maker,model){
+    this.maker = maker
+    this.model = model
+}
+   let myPerson = new Person ('shehzad', 28);
+   let myCar = new Car('DogeChallenger', 1969)
+   console.log(myPerson);
+   console.log(myCar.model);
+
+function Store (ice_cream){
+    this.ice_cream = ice_cream
+    this.vanila = function (){
+        return `the icecream i want is ${ice_cream}`
     }
 }
-
-let a = new Teatype();
-// console.log(a.des());
-
-
-function Animal(species){
-    this.species = species
-}
-Animal.prototype.sound = function(){
-    return `${this.species} make a sound`
-}
-let dog = new Animal('dog');
-console.log(dog.sound());
+   let Result =  new Store('vanilla')
+   console.log(Result.vanila())
