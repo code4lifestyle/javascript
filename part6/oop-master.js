12e8ab8be7cfd8869fe559527d23d1e5ef275bd8
Code4life <code4lifestyle@gmail.com>
2025-09-19 03:25:30 +0500

    encapsulation

--- part6/oop-master.js
+++ part6/oop-master.js
@@ -1,22 +1,32 @@
-// Encapsulation 
-class vechileC1 {
-    // proparties
-    name;
-    color;
-    wheels;
-    #regNumber;
-    // constructor
-    constructor(name,color,wheels, regnumber){
-        this.name = name;
-        this.color = color;
-        this.wheels = wheels;
-        this.#regNumber = regnumber;
-    };
-    // methods
-    getDetails(){
-        console.log(`the ${this.name} is ${this.color} in color, it has ${this.wheels} wheels its registration is ${this.#regNumber}`)
-    };
-    };
-   const veh1 = new vechileC1('scooter', 'grey', 3,222 );
-   console.log(veh1)
- veh1.getDetails()
+// // Encapsulation 
+// class vechileC1 {
+//     // proparties
+//     name;
+//     color;
+//     wheels;
+//     #regNumber;
+//     // constructor
+//     constructor(name,color,wheels, regnumber){
+//         this.name = name;
+//         this.color = color;
+//         this.wheels = wheels;
+//         this.#regNumber = regnumber;
+//     };
+//     // methods
+//     getDetails(){
+//         console.log(`the ${this.name} is ${this.color} in color, it has ${this.wheels} wheels its registration is ${this.#regNumber}`)
+//     };
+//     };
+//    const veh1 = new vechileC1('scooter', 'grey', 3,222 );
+//    console.log(veh1)
+//  veh1.getDetails()
+
+let car = {
+    make : 'Doge',
+    model : 'black ghoost',
+    year : 1970,
+    start : function(){
+         return `${this.make} car got started in ${this.year} model ${this.model} is Realy Awesome`
+    }
+}
+// console.log(car.start());
