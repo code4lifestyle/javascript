1f6addae499dcc68d7aa8458a1569a2d3a64dc97
Code4life <code4lifestyle@gmail.com>
2025-09-19 03:08:46 +0500

    encapsulation

--- part6/oop-master.js
+++ part6/oop-master.js
@@ -4,16 +4,19 @@ class vechileC1 {
     name;
     color;
     wheels;
+    #regNumber;
     // constructor
-    constructor(name,color,wheels){
+    constructor(name,color,wheels, regnumber){
         this.name = name;
         this.color = color;
-        this.wheels = wheels
+        this.wheels = wheels;
+        this.#regNumber = regnumber;
     };
     // methods
     getDetails(){
-        console.log(`the ${this.name} is ${this.color} in color, it has ${this.wheels} wheels`)
+        console.log(`the ${this.name} is ${this.color} in color, it has ${this.wheels} wheels its registration is ${this.#regNumber}`)
     };
     };
-   const veh1 = new vechileC1('scooter', 'grey', 3);
+   const veh1 = new vechileC1('scooter', 'grey', 3,222 );
    console.log(veh1)
+ veh1.getDetails()
