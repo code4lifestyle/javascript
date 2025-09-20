baa3bff9e71c494487f0db61cb67acd5723a245a
Code4life <code4lifestyle@gmail.com>
2025-09-21 01:51:36 +0500

    exercise

--- part7/script.js
+++ part7/script.js
@@ -1,4 +1,13 @@
-document.getElementById("changeTextButton").addEventListener('click', function(){
-   let paragraph =  document.getElementById('myParagraph');
+document.getElementById("changeTextButton")
+.addEventListener('click', function(){
+   let paragraph =  document.getElementById
+   ('myParagraph');
     paragraph.textContent = "this peragraph is changed"
+});
+
+// Example 2
+document.getElementById('highlighFirstCity').addEventListener('click', function(){
+   let Cities =  document.getElementById('citieslist')
+   (citieslist.firstElementChild.classList.add('highlight'))
+
 })
