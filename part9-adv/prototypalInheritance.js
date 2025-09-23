2aa076f0ecde897d38baaa6dab45db994bf283e8
Code4life <code4lifestyle@gmail.com>
2025-09-24 04:21:14 +0500

    proto

--- part9-adv/prototypalInheritance.js
+++ part9-adv/prototypalInheritance.js
@@ -6,3 +6,24 @@
 // }
 // let personNAme = new Person('personNAme')
 // personNAme.greet();
+
+// const p1 = {
+//     fname :'jhon',
+//     lname : "Dov",
+//     getFullname(){
+//         return `${this.fname} ${this.lname}`
+//     }
+// }
+// const p2 = Object.create(p1)
+// p2.__proto__.fname = "ali"
+// console.log("p1 is", p1.getFullname());
+// console.log("p2 is", p2.getFullname());
+// console.log("replace value", p1.fname)
+const p1 = {
+    xp1 : "i am inside p1"
+}
+const p2 = {
+    xp2 : "i am inside p2",
+    __proto__ : p1
+}
+console.log(p2)
