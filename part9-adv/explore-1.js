19b69fe277030d895840e5cd96f461c00a25311f
Code4life <code4lifestyle@gmail.com>
2025-09-23 19:46:16 +0500

    revision

--- part9-adv/explore-1.js
+++ part9-adv/explore-1.js
@@ -1,7 +1,12 @@
-console.log('testing');
-setTimeout(()=>{
-    console.log('i want to say hello')
-},4000)
-for (let index = 0; index < 10; index++) {
-  console.log(index)
+// console.log('testing');
+// setTimeout(()=>{
+//     console.log('i want to say hello')
+// },4000)
+// for (let index = 0; index < 10; index++) {
+//   console.log(index)
+// }
+
+console.log('hello world')
+for (i=0; i< 10; i++){
+  console.log(i)
 }
