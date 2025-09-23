bd23b3eb926a15d57dbc46482895637967b642ed
Code4life <code4lifestyle@gmail.com>
2025-09-24 00:15:26 +0500

    fetch data

--- part9-adv/promise.js
+++ part9-adv/promise.js
@@ -1,7 +1,7 @@
 function fetchData(){
     return new Promise((resolve, reject)=>{
         setTimeout(()=>{
-            let success = true;
+            let success = false;
             if(success){
                 resolve("data fetched successfully")
             }else{
@@ -10,16 +10,24 @@ function fetchData(){
         },4000)
     })
 }
+        fetchData()
+        .then((data)=> console.log(data))
+        .catch((error)=> console.log(error))
 
-function UpdateData(){
-    return new Promise(()=>{
-        setTimeout(()=>{
-            let success = true;
-            if (success){
-                resolve('Updated successfully')
-            }else{
-                reject('There is an Error')
-            }
-        },4000)
-    })
-}
+
+// function UpdateData(){
+//     return new Promise(()=>{
+//         setTimeout(()=>{
+//             let success = true;
+//             if (success){
+//                 resolve('Updated successfully')
+//             }else{
+//                 reject('There is an Error')
+//             }
+//         },7000)
+//     })
+// }
+// let Responce = fetchData();
+// console.log(Responce)
+// let update = UpdateData();
+// console.log(update);
