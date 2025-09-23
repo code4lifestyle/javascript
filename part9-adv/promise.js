390f1a10d8fdba3f301194843db31b3b6a62701f
Code4life <code4lifestyle@gmail.com>
2025-09-24 01:07:38 +0500

    fetch Api

--- part9-adv/promise.js
+++ part9-adv/promise.js
@@ -39,3 +39,16 @@
 // console.log(Responce)
 // let update = UpdateData();
 // console.log(update);
+
+let ResultfromServer = fetch(
+    "https://jsonplaceholder.typicode.com/todos"
+).then((response)=>{
+    if(!response.ok) throw console.error('responce is not OK');
+    return response.json();
+}).then((json) => {
+    todos = json.slice(0, 5); // Store first 10 items
+    todos.forEach((todo, index) => {
+      console.log(`Todo ${index + 1}: ID = ${todo.id}, Title = ${todo.title}, completed = ${todo.completed}`);
+    });
+  })
+  .catch((error) => console.error("Error fetching data:", error));
