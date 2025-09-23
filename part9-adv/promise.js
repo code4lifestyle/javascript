090ca8ec19a8fdea1fae7ae1028e47ee87810dbb
Code4life <code4lifestyle@gmail.com>
2025-09-24 00:42:44 +0500

    changes

--- part9-adv/promise.js
+++ part9-adv/promise.js
@@ -1,18 +1,26 @@
-function fetchData(){
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            let success = false;
-            if(success){
-                resolve("data fetched successfully")
-            }else{
-                reject('There is an Error')
-            }
-        },4000)
-    })
-}
-        fetchData()
-        .then((data)=> console.log(data))
-        .catch((error)=> console.log(error))
+// function fetchData(){
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             let success = false;
+//             if(success){
+//                 resolve("data fetched successfully")
+//             }else{
+//                 reject('There is an Error')
+//             }
+//         },4000)
+//     })
+// }
+//         fetchData()
+//         .then((data)=> console.log(data))
+//         .catch((error)=> console.log(error))
+
+// fetch('https://jsonplaceholder.typicode.com/todos')
+//   .then(response => {
+//     if (!response.ok) throw new Error('Network response was not ok');
+//     return response.json();
+//   })
+//   .then(json => console.log(json))
+//   .catch(error => console.error('Error fetching data:', error));
 
 
 // function UpdateData(){
