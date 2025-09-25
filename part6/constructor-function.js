6db7581213328c1eedbe065acfab18c9a9c913b8
Code4life <code4lifestyle@gmail.com>
2025-09-25 23:43:51 +0500

    updated

--- part6/constructor-function.js
+++ part6/constructor-function.js
@@ -1,53 +1,53 @@
-// function Person(name,age){
-//     this.name = name,
-//     this.age = age
-// }
-// function Car(maker,model){
-//     this.maker = maker,
-//     this.model = model
-// }
-// let myPerson = new Person('shehzad',29)
-// let myCar = new Car("toyota", "camry")
-// // console.log(myCar);
-// // console.log(myPerson);
-// function Tea(type){
-//     this.type = type;
-//     this.describe = function(){
-//         return `this is cup of ${this.type}`;
-//     }
-// }
-// let lemonTea = new Tea('lemon Tea');
-// console.log(lemonTea.describe());
+function Person(name,age){
+    this.name = name,
+    this.age = age
+}
+function Car(maker,model){
+    this.maker = maker,
+    this.model = model
+}
+let myPerson = new Person('shehzad',29)
+let myCar = new Car("toyota", "camry")
+// console.log(myCar);
+// console.log(myPerson);
+function Tea(type){
+    this.type = type;
+    this.describe = function(){
+        return `this is cup of ${this.type}`;
+    }
+}
+let lemonTea = new Tea('lemon Tea');
+console.log(lemonTea.describe());
 
 
-// function Tea(type){
-//     this.type = type ;
-//     this.describe = function(){
-//         return `the name is ${type}`
-//     }
-// }
-// let TeaType = new Tea('black tea')
-// console.log(TeaType.describe())
+function Tea(type){
+    this.type = type ;
+    this.describe = function(){
+        return `the name is ${type}`
+    }
+}
+let TeaType = new Tea('black tea')
+console.log(TeaType.describe())
 
-// function Teatype(type) {
-//     this.type = "any"; 
-//     this.des = function() {
-//         return `the key name is ${this.type}`;
-//     }
-// }
+function Teatype(type) {
+    this.type = "any"; 
+    this.des = function() {
+        return `the key name is ${this.type}`;
+    }
+}
 
-// let a = new Teatype();
-// // console.log(a.des());
+let a = new Teatype();
+// console.log(a.des());
 
 
-// function Animal(species){
-//     this.species = species
-// }
-// Animal.prototype.sound = function(){
-//     return `${this.species} make a sound`
-// }
-// let dog = new Animal('dog');
-// console.log(dog.sound());
+function Animal(species){
+    this.species = species
+}
+Animal.prototype.sound = function(){
+    return `${this.species} make a sound`
+}
+let dog = new Animal('dog');
+console.log(dog.sound());
 
 function Person (name,age){
     this.name = name
