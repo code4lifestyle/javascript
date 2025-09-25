7f6abbe08a2f744a0267ea7029a42df965a88b04
Code4life <code4lifestyle@gmail.com>
2025-09-25 23:43:13 +0500

    updated

--- part9-adv/thisContext.js
+++ part9-adv/thisContext.js
@@ -1,16 +1,16 @@
-// const person = {
-//     name : 'shehzad',
-//     greet(){
-//         console.log(`hey my name is ${this.name}`)
-//     },
+const person = {
+    name : 'shehzad',
+    greet(){
+        console.log(`hey my name is ${this.name}`)
+    },
     
-// };
-// person.greet();
-// const GreetFunction = person.greet;
-// GreetFunction();
-// // ther person name will be undefined in greetfunction
-// const bindGreet = person.greet.bind({name:'jhone'});
-// bindGreet();
+};
+person.greet();
+const GreetFunction = person.greet;
+GreetFunction();
+// ther person name will be undefined in greetfunction
+const bindGreet = person.greet.bind({name:'jhone'});
+bindGreet();
 const User = {
     name : 'jhon',
     email : "jhone@gmail.com",
