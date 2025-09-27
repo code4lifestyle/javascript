b4f1e9674d4dd4125eebde6dd3ab29280df88226
Code4life <code4life>
2025-09-28 04:38:05 +0500

    changes

--- part9-adv/asyncAwait2.js
+++ part9-adv/asyncAwait2.js
@@ -1,34 +1,49 @@
-function fetchPostData(){
+// function fetchPostData(){
+//     return new Promise((resolve)=>{
+//         setTimeout(() => {
+//             resolve("Post data fetchpostdata")
+//         }, 2000);
+//     });
+// }
+
+// function fetchCommentData(){
+//     return new Promise((resolve)=>{
+//     setTimeout(() => {
+//             resolve("comment data fetched fetchcommentdata")
+//     }, 3000);
+//     });
+// }
+
+// async function getBlogData(){
+//     try{
+//         console.log("fetch blog data getfetchBlogData");
+//         // const BlogData = await fetchPostData();
+//         // const commentData = await fetchCommentData();
+//         // instead of this we can use promise all
+//         const [posData, commentData]= await Promise.all([
+//             fetchPostData(), fetchCommentData(),
+
+//         ]);
+//         console.log(posData);
+//         console.log(commentData)
+//         console.log("fetch complete")
+//     }catch(error){
+//         console.log("error fetching blog  errorgetfetchBlog",error)
+//     }
+// }
+// getBlogData();
+
+function resolveAfter2second(){
     return new Promise((resolve)=>{
         setTimeout(() => {
-            resolve("Post data fetchpostdata")
+            resolve("resolved")
         }, 2000);
-    });
+    })
 }
 
-function fetchCommentData(){
-    return new Promise((resolve)=>{
-    setTimeout(() => {
-            resolve("comment data fetched fetchcommentdata")
-    }, 3000);
-    });
+async function asyncCall(){
+    console.log("calling");
+    const result = await resolveAfter2second();
+    console.log("result is :", result);
 }
-
-async function getBlogData(){
-    try{
-        console.log("fetch blog data getfetchBlogData");
-        // const BlogData = await fetchPostData();
-        // const commentData = await fetchCommentData();
-        // instead of this we can use promise all
-        const [posData, commentData]= await Promise.all([
-            fetchPostData(), fetchCommentData(),
-
-        ]);
-        console.log(posData);
-        console.log(commentData)
-        console.log("fetch complete")
-    }catch(error){
-        console.log("error fetching blog  errorgetfetchBlog",error)
-    }
-}
-getBlogData();
+asyncCall();
