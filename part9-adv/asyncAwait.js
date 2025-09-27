0dbd8bb5a3e73edc0e206e528a840e28ca2520f5
Code4life <code4life>
2025-09-28 01:24:30 +0500

    changes

--- part9-adv/asyncAwait.js
+++ part9-adv/asyncAwait.js
@@ -1,7 +1,20 @@
-// function fetchData(){
-//     return new Promise ((resolve, reject)=>{
-//         setTimeout(() => {
-//             resolve({nameL : "abc", url:"xyz"})
-//         }, 3000);
-//     })
-// }
+function fetchUserData(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            resolve({name:"jhin Dovee", url:"https://chaicode.com"})
+        }, 3000);
+    })
+}
+
+
+async function getUserData(){
+            try{
+                console.log("fetching userData..........");
+                const userData = await getUserData();
+                console.log("user data fetched successfully")
+                console.log("user data", userData )
+                }catch(error){
+                console.log("error in fetching Data", error)
+            };
+            }
+            getUserData();
