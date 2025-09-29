69be62babcd4dd9db7c8614bf2f7cec4638fad08
Code4life <code4lifestyle@gmail.com>
2025-09-29 18:30:12 +0500

    new changes global

--- part6/oop-master.js
+++ part6/oop-master.js
@@ -1,67 +1,67 @@
-// // Encapsulation 
-class vechileC1 {
-    // proparties
-    name;
-    color;
-    wheels;
-    #regNumber;
-    // constructor
-    constructor(name,color,wheels, regnumber){
-        this.name = name;
-        this.color = color;
-        this.wheels = wheels;
-        this.#regNumber = regnumber;
-    };
-    // methods
-    getDetails(){
-        console.log(`the ${this.name} is ${this.color} in color, it has ${this.wheels} wheels its registration is ${this.#regNumber}`)
-    };
-    };
-   const veh1 = new vechileC1('scooter', 'grey', 3,222 );
-   console.log(veh1)
- veh1.getDetails()
+// // // Encapsulation 
+// class vechileC1 {
+//     // proparties
+//     name;
+//     color;
+//     wheels;
+//     #regNumber;
+//     // constructor
+//     constructor(name,color,wheels, regnumber){
+//         this.name = name;
+//         this.color = color;
+//         this.wheels = wheels;
+//         this.#regNumber = regnumber;
+//     };
+//     // methods
+//     getDetails(){
+//         console.log(`the ${this.name} is ${this.color} in color, it has ${this.wheels} wheels its registration is ${this.#regNumber}`)
+//     };
+//     };
+//    const veh1 = new vechileC1('scooter', 'grey', 3,222 );
+//    console.log(veh1)
+//  veh1.getDetails()
 
-let car = {
-    make : 'Doge',
-    model : 'black ghoost',
-    year : 1970,
-    start : function(){
-         return `${this.make} car got started in ${this.year} model ${this.model} is Realy Awesome`
-    }
-}
-// console.log(car.start());
+// let car = {
+//     make : 'Doge',
+//     model : 'black ghoost',
+//     year : 1970,
+//     start : function(){
+//          return `${this.make} car got started in ${this.year} model ${this.model} is Realy Awesome`
+//     }
+// }
+// // console.log(car.start());
 
-function Person(name,age){
-    this.name = name
-    this.age = age
-}
-let jhon  = new Person('jhon','23');
-// console.log(jhon.age)
+// function Person(name,age){
+//     this.name = name
+//     this.age = age
+// }
+// let jhon  = new Person('jhon','23');
+// // console.log(jhon.age)
 
-function Animal(type){
-    this.type = type
-}
-Animal.prototype.speak = function(){
-    return `${this.type} make a sound`
-}
-Array.prototype.shehzad = function(){
-    return `custom method ${this}`
-    }
-let myArray = [1,2,3,4];
-// console.log(myArray.shehzad())
-let mynewArray = [1,2,3,4,5,6,7];
-// console.log(mynewArray.shehzad())
+// function Animal(type){
+//     this.type = type
+// }
+// Animal.prototype.speak = function(){
+//     return `${this.type} make a sound`
+// }
+// Array.prototype.shehzad = function(){
+//     return `custom method ${this}`
+//     }
+// let myArray = [1,2,3,4];
+// // console.log(myArray.shehzad())
+// let mynewArray = [1,2,3,4,5,6,7];
+// // console.log(mynewArray.shehzad())
 
-class Vechile{
-    constructor(make,model){
-    this.make = make
-    this.model = model
-    }
-    //method
-    start       (){
-        return `this ${this.model}  car is from ${this.make}`
-    }
-}
+// class Vechile{
+//     constructor(make,model){
+//     this.make = make
+//     this.model = model
+//     }
+//     //method
+//     start       (){
+//         return `this ${this.model}  car is from ${this.make}`
+//     }
+// }
 
 class Car extends Vechile {
     drive(){
