b91d2424a8c5f05f87ba99398c4407fcc356deb7
Code4life <code4lifestyle@gmail.com>
2025-10-02 22:26:45 +0500

    async await

--- part9-adv/asyncAwait.js
+++ part9-adv/asyncAwait.js
@@ -1,23 +1,23 @@
-// function fetchUserData(){
-//     return new Promise((resolve, reject)=>{
-//         setTimeout(() => {
-//             reject({name:"jhin Dovee", url:"https://chaicode.com"})
-//         }, 3000);
-//     })
-// }
+function fetchUserData(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            reject({name:"jhin Dovee", url:"https://chaicode.com"})
+        }, 3000);
+    })
+}
 
 
-// async function getUserData(){
-//             try{
-//                 console.log("fetching userData..........");
-//                 const userData = await fetchUserData();
-//                 console.log("user data fetched successfully")
-//                 console.log("user data", userData )
-//                 }catch(error){
-//                 console.log("error in fetching Data", error)
-//             };
-//             }
-//             getUserData();
+async function getUserData(){
+            try{
+                console.log("fetching userData..........");
+                const userData = await fetchUserData();
+                console.log("user data fetched successfully")
+                console.log("user data", userData )
+                }catch(error){
+                console.log("error in fetching Data", error)
+            };
+            }
+            getUserData();
 
     function fetchUserData(){
         return new Promise((resolve, reject)=>{
--- part9-adv/asyncAwait2.js
+++ part9-adv/asyncAwait2.js
@@ -1,72 +1,72 @@
-// function fetchPostData(){
-//     return new Promise((resolve)=>{
-//         setTimeout(() => {
-//             resolve("Post data fetchpostdata")
-//         }, 2000);
-//     });
-// }
+function fetchPostData(){
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+            resolve("Post data fetchpostdata")
+        }, 2000);
+    });
+}
 
-// function fetchCommentData(){
-//     return new Promise((resolve)=>{
-//     setTimeout(() => {
-//             resolve("comment data fetched fetchcommentdata")
-//     }, 3000);
-//     });
-// }
+function fetchCommentData(){
+    return new Promise((resolve)=>{
+    setTimeout(() => {
+            resolve("comment data fetched fetchcommentdata")
+    }, 3000);
+    });
+}
 
-// async function getBlogData(){
-//     try{
-//         console.log("fetch blog data getfetchBlogData");
-//         // const BlogData = await fetchPostData();
-//         // const commentData = await fetchCommentData();
-//         // instead of this we can use promise all
-//         const [posData, commentData]= await Promise.all([
-//             fetchPostData(), fetchCommentData(),
+async function getBlogData(){
+    try{
+        console.log("fetch blog data getfetchBlogData");
+        // const BlogData = await fetchPostData();
+        // const commentData = await fetchCommentData();
+        // instead of this we can use promise all
+        const [posData, commentData]= await Promise.all([
+            fetchPostData(), fetchCommentData(),
 
-//         ]);
-//         console.log(posData);
-//         console.log(commentData)
-//         console.log("fetch complete")
-//     }catch(error){
-//         console.log("error fetching blog  errorgetfetchBlog",error)
-//     }
-// }
-// getBlogData();
+        ]);
+        console.log(posData);
+        console.log(commentData)
+        console.log("fetch complete")
+    }catch(error){
+        console.log("error fetching blog  errorgetfetchBlog",error)
+    }
+}
+getBlogData();
 
-// function resolveAfter2second(){
-//     return new Promise((resolve)=>{
-//         setTimeout(() => {
-//             resolve("resolved")
-//         }, 2000);
-//     })
-// }
+function resolveAfter2second(){
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+            resolve("resolved")
+        }, 2000);
+    })
+}
 
-// async function asyncCall(){
-//     console.log("calling");
-//     const result = await resolveAfter2second();
-//     console.log("result is :", result);
-// }
-// asyncCall();
+async function asyncCall(){
+    console.log("calling");
+    const result = await resolveAfter2second();
+    console.log("result is :", result);
+}
+asyncCall();
 
-// function fetchuserData(){
-//     return new Promise((resolve,reject)=>{
-//     setTimeout(() => {
-//         resolve({name: "jhon", url:'https://fakestoreapi.com/products'})
-//     }, 3000);
-//     })
-// }
+function fetchuserData(){
+    return new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        resolve({name: "jhon", url:'https://fakestoreapi.com/products'})
+    }, 3000);
+    })
+}
 
-// async function getUserData(){
-//     try{
-//         console.log("fetching user Data........");
-//         const userData = await fetchuserData();
-//         console.log("fetched successfully")
-//         console.log("User data :",userData);
-//     } catch (error){
-//         console.log("there is an error", error);
-//     }
-// }
-// getUserData();
+async function getUserData(){
+    try{
+        console.log("fetching user Data........");
+        const userData = await fetchuserData();
+        console.log("fetched successfully")
+        console.log("User data :",userData);
+    } catch (error){
+        console.log("there is an error", error);
+    }
+}
+getUserData();
 
 function fetchmsgData(){
     return new Promise((resolve, reject)=>{
