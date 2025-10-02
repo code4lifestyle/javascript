54298bdc4f825f8da5cd9b2d4497ac9b94c6c3b2
Code4life <code4lifestyle@gmail.com>
2025-10-02 23:01:22 +0500

    changes proptypeinheritance

--- part9-adv/prototypalInheritance.js
+++ part9-adv/prototypalInheritance.js
@@ -1,24 +1,24 @@
-// function Person(name){
-//     this.name = name 
-// }
-// Person.prototype.greet = function(){
-//     console.log(`Hello my name is ${this.name}`)
-// }
-// let personNAme = new Person('personNAme')
-// personNAme.greet();
+function Person(name){
+    this.name = name 
+}
+Person.prototype.greet = function(){
+    console.log(`Hello my name is ${this.name}`)
+}
+let personNAme = new Person('personNAme')
+personNAme.greet();
 
-// const p1 = {
-//     fname :'jhon',
-//     lname : "Dov",
-//     getFullname(){
-//         return `${this.fname} ${this.lname}`
-//     }
-// }
-// const p2 = Object.create(p1)
-// p2.__proto__.fname = "ali"
-// console.log("p1 is", p1.getFullname());
-// console.log("p2 is", p2.getFullname());
-// console.log("replace value", p1.fname)
+const p1 = {
+    fname :'jhon',
+    lname : "Dov",
+    getFullname(){
+        return `${this.fname} ${this.lname}`
+    }
+}
+const p2 = Object.create(p1)
+p2.__proto__.fname = "ali"
+console.log("p1 is", p1.getFullname());
+console.log("p2 is", p2.getFullname());
+console.log("replace value", p1.fname)
 const p1 = {
     xp1 : "i am inside p1"
 }
