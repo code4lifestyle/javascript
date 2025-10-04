5a8db1dab70d8fea751c9ac3f32b55fc5d42fbd4
Code4life <code4lifestyle@gmail.com>
2025-10-04 22:25:32 +0500

    changes

--- part9-adv/appM.js
+++ part9-adv/appM.js
@@ -1,5 +1,9 @@
 // import default
 import multiply from "./mathOperationsM.js";
 
+// name import
+import {add, substract} from './mathOperationsM.js'
 
 console.log(multiply(2,2));
+console.log(add(2,3));
+console.log(substract(5,1233));
