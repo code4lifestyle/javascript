ffb934e2af19812e15817d8833d3448fd7e4dab9
Code4life <code4lifestyle@gmail.com>
2025-10-05 02:32:27 +0500

    take data from input

---
+++ 01-todoApplocalstorage/script.js
@@ -0,0 +1,18 @@
+const todoInput = document.getElementById("todo-input")
+const addTask = document.getElementById("add-task-btn")
+const todoList = document.getElementById("todo-list")
+
+let tasks = [];
+
+addTask.addEventListener('click', () => {
+    const tasktext = todoInput.value.trim();
+    if(tasktext ==="")return;
+    const newTask = {
+        id: Date.now(),
+        text: tasktext,
+        completed : false
+    }
+    tasks.push(newTask)
+    todoInput.Value = ""
+    console.log(tasks)
+})
