77e8ef401553f5dbf5ca4153ccf52a1a38a1c795
Code4life <code4lifestyle@gmail.com>
2025-10-07 01:10:53 +0500

    changes

--- 01-todoApplocalstorage/script.js
+++ 01-todoApplocalstorage/script.js
@@ -1,22 +1,36 @@
-const todoInput = document.getElementById('todo-input')
-const addTaskButton = document.getElementById('add-task-btn')
-const todoList = document.getElementById('todo-list')
+document.addEventListener('DOMContentLoaded', () => {
 
+const todoInput = document.getElementById("todo-input");
+const addTaskButton = document.getElementById("add-task-btn");
+const todoList = document.getElementById("todo-list");
 
-let tasks = []
+let tasks =  JSON.parse(localStorage.getItem('tasks')) || [];
+tasks.forEach(task => renderTask(task))
 
-addTaskButton.addEventListener('click', () => {
-   const taskText =  todoInput.value.trim()
-   if(taskText === "") return;
+addTaskButton.addEventListener("click", () => {
+  const taskText = todoInput.value.trim();
+  if (taskText === "") return;
 
   //  taking input data create object for that
-   const newTask = {
-    id : Date.now(),
-    text : taskText,
-    completed : false
-   }
+  const newTask = {
+    id: Date.now(),
+    text: taskText,
+    completed: false,
+  };
+
   //  pushing newTask to array task
-const result = tasks.push(newTask)
- todoInput.value = ""
-   console.log("new task is : ", newTask)
-})
+  tasks.push(newTask);
+     saveTask();
+  todoInput.value = "";
+  console.log("new task is : ", newTask);
+});
+
+  function renderTask(task){
+    console.log(task.text)
+  }
+
+function saveTask() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+});
