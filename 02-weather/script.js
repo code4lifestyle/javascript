f8ffb1caa0c2c2936da7be9088e5d51b43766953
Code4life <code4lifestyle@gmail.com>
2025-10-07 03:59:48 +0500

    weather

---
+++ 02-weather/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const cityInput =  document.getElementById('city-input');
+  const getWeatherBtn =  document.getElementById('get-weather-btn');
+  const weatherInfo = document.getElementById('weather-info');
+  const cityNameDisplay = document.getElementById('city-name');
+  const tempratureDisplay = document.getElementById('temperature');
+  const descriptionDisplay = document.getElementById('description')
+  const errorMessage = document.getElementById('error-message');
+
+    getWeatherBtn.addEventListener('click', () => {
+        const city = cityInput.value.trim();
+        if(!city) return;
+    })
+
+
+})
