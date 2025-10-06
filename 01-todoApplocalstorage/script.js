dd940fcbabaf364f86b0f85a66eac0699a811327
Code4life <code4lifestyle@gmail.com>
2025-10-07 00:42:57 +0500

    taking data from input

--- 01-todoApplocalstorage/script.js
+++ 01-todoApplocalstorage/script.js
@@ -1,42 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const todoInput = document.getElementById("todo-input");
-  const addTask = document.getElementById("add-task-btn");
-  const todoList = document.getElementById("todo-list");
+const todoInput = document.getElementById('todo-input')
+const addTaskButton = document.getElementById('add-task-btn')
+const todoList = document.getElementById('todo-list')
 
-  let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-  tasks.forEach(task => renderTask(task))
 
-  addTask.addEventListener("click", () => {
-    const tasktext = todoInput.value.trim();
-    if (tasktext === "") return;
-    const newTask = {
-      id: Date.now(),
-      text: tasktext,
-      completed: false,
-    };
-    tasks.push(newTask);
-    saveTasks();
-    todoInput.value = "";
-    console.log(tasks);
-  });
+let tasks = []
 
-  function renderTask(task) {
-    const li = document.createElement('li')
-    li.setAttribute('data-id', task.id)
-    if(task.completed) li.classList.add('completed')
-    li.innerHTML = `
-    <span>${task.text}</span>
-    <button>delete</button>`;
-    li.addEventListener('click', (e) => {
-        if(e.target.tagName === 'BUTTON') return
-        task.completed = !task.completed
-        li.classList.toggle('completed')
-        saveTasks()
-    })
-    todoList.appendChild(li);
-  }
+addTaskButton.addEventListener('click', () => {
+   const taskText =  todoInput.value.trim()
+   if(taskText === "") return;
 
-  function saveTasks() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }
-});
+  //  taking input data create object for that
+   const newTask = {
+    id : Date.now(),
+    text : taskText,
+    completed : false
+   }
+  //  pushing newTask to array task
+const result = tasks.push(newTask)
+ todoInput.value = ""
+   console.log("new task is : ", newTask)
+})
