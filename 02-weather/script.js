d4182c048bb1bb48ce571a52c5a21e1c630d0ad7
code4lifestyle <code4lifestyle@gmail.com>
2025-10-08 02:53:46 +0500

    event listner on getweatherbtn

--- 02-weather/script.js
+++ 02-weather/script.js
@@ -7,35 +7,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const descriptionDisplay = document.getElementById('description')
   const errorMessage = document.getElementById('error-message');
 
-    const API_KEY = "739701c972097e496b9275d8cffa665f"
-    getWeatherBtn.addEventListener('click', async () => {
-        const city = cityInput.value.trim();
-        if(!city) return;
-
-        try {
-          const weatherData = await fetchWeatherData();
-          displayWeatherData(weatherData)
-        } catch(error){
-          showError();
-        }
-    })
-    async function fetchWeatherData(city){
-      const url = `https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=${API_KEY}`;
-      const response = await fetch(url);
-      console.log(typeof response);
-      console.log("response  is :", response);
-      if(!response.ok){
-        throw new Error('City not found');
-      }
-      const data = await response.json();
-      return data;
-    }
-    function displayWeatherData(weatherData){
-      console.log("weather data is :", weatherData);
-    }
-    function showError(){
-      weatherInfo.classList.add('hidden');
-      errorMessage.classList.remove('hidden')
-    }
+   getWeatherBtn.addEventListener('click', () => {
+    const cityName = cityInput.value.trim();
+    if(!city) return;
+      
+   })
 
 })
