8abd98df262263e5e436b5fd069ed53322669a5a
code4lifestyle <code4lifestyle@gmail.com>
2025-10-08 01:28:21 +0500

    fetch api

--- 02-weather/script.js
+++ 02-weather/script.js
@@ -20,10 +20,18 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
     async function fetchWeatherData(city){
-
+      const url = `https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid={API_KEY}`;
+      const response = await fetch(url);
+      console.log(typeof response);
+      console.log("response  is :", response);
+      if(!response.ok){
+        throw new Error('City not found');
+      }
+      const data = await response.json();
+      return data;
     }
     function displayWeatherData(weatherData){
-      
+      console.log("weather data is :", weatherData);
     }
     function showError(){
       weatherInfo.classList.add('hidden');
