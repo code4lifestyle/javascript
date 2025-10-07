76615a548d3260a7eddc07d04e4776b6a7ff9900
code4lifestyle <code4lifestyle@gmail.com>
2025-10-08 03:03:26 +0500

    error handling

--- 02-weather/script.js
+++ 02-weather/script.js
@@ -6,11 +6,24 @@ document.addEventListener('DOMContentLoaded', () => {
   const tempratureDisplay = document.getElementById('temperature');
   const descriptionDisplay = document.getElementById('description')
   const errorMessage = document.getElementById('error-message');
-
+  const API_KEY = "fe7d5b917e2d7670f94be08dfbe71881"
    getWeatherBtn.addEventListener('click', () => {
     const cityName = cityInput.value.trim();
     if(!city) return;
-      
-   })
+
+   });
+
+   function fetchWeatherData(city){
+
+   }
+
+   function displayWeatherData(weatherData){
+
+   }
+
+   function showWeatherData(){
+    weatherInfo.classList.add('hidden');
+    errorMessage.classList.remove('hidden')
+   }
 
 })
