aab1a94f61cef83bde08d9b5f426e2802fe10c88
code4lifestyle <code4lifestyle@gmail.com>
2025-10-09 03:59:11 +0500

    changes

--- 02-weather/script.js
+++ 02-weather/script.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const tempratureDisplay = document.getElementById('temperature');
   const descriptionDisplay = document.getElementById('description')
   const errorMessage = document.getElementById('error-message');
+
   const API_KEY = "fe7d5b917e2d7670f94be08dfbe71881"
    getWeatherBtn.addEventListener('click', () => {
     const cityName = cityInput.value.trim();
@@ -14,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
    });
 
    function fetchWeatherData(city){
-
+      console.log(city);
    }
 
    function displayWeatherData(weatherData){
