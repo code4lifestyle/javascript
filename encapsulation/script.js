f0b22c87a65fa5eb143fef29e4af96fae07c004c
code4lifestyle <code4lifestyle@gmail.com>
2025-10-15 22:08:44 +0500

    changes

--- encapsulation/script.js
+++ encapsulation/script.js
@@ -1,20 +1,20 @@
-// const User = {
-//     name : 'shehzad',
-//     lastName : 'Shah',
-//     age : 28,
+const User = {
+    name : 'shehzad',
+    lastName : 'Shah',
+    age : 28,
 
-// }
+}
 function getAgeYear(age){
    return new Date().getFullYear() - age;
 
 }
 // using encapsulation it will be
 
-const User ={
-    name : 'shehzad',
-    lastName : 'shah',
-    age : 28,
-    getAgeYear : function (){
-        return new Date().getFullYear() - User.age
-    }
-}
+// const User ={
+//     name : 'shehzad',
+//     lastName : 'shah',
+//     age : 28,
+//     getAgeYear : function (){
+//         return new Date().getFullYear() - User.age
+//     }
+// }
