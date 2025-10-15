f1ed785f124cddb72cbd8bf8f0cfd5b531058261
code4lifestyle <code4lifestyle@gmail.com>
2025-10-15 22:05:39 +0500

    undochanges

--- 02-weather/script.js
+++ 02-weather/script.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const cityInput =  document.getElementById('city-input');
   const getWeatherBtn =  document.getElementById('get-weather-btn');
   const weatherInfo = document.getElementById('weather-info');
-//   const cityNameDisplay = document.getElementById('city-name');
-//   const tempratureDisplay = document.getElementById('temperature');
-//   const descriptionDisplay = document.getElementById('description')
+  const cityNameDisplay = document.getElementById('city-name');
+  const tempratureDisplay = document.getElementById('temperature');
+  const descriptionDisplay = document.getElementById('description')
   const errorMessage = document.getElementById('error-message');
 
   const API_KEY = "fe7d5b917e2d7670f94be08dfbe71881"
